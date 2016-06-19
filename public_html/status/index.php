ad979379758c396500494d2a3f75f39bb9edc437
Ryan <ryanf.rowe@gmail.com>
2016-06-19 13:52:05 -0400

    Added initial status page

---
+++ public_html/status/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require($_SERVER["DOCUMENT_ROOT"]."/utils/UserUtils.php");
+$userutils = new UserUtils();
+
+?>
+<html>
+    <head>
+        <title>rynet status</title>
+    </head>
+    <body>
+        <?php include($_SERVER["DOCUMENT_ROOT"]."/utils/header.php") ?>
+        <h2>Welcome, <?php echo $userutils->username() ?></h2>
+    </body>
+</html>

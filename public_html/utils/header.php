13f9ac8a4c38b951960693f5e4301bb485027712
Ryan <ryanf.rowe@gmail.com>
2016-06-19 13:51:26 -0400

    Made logo clickable and added future css identifiers

--- public_html/utils/header.php
+++ public_html/utils/header.php
@@ -1,8 +1,8 @@
 <header>
-    <h1 id="logo">rynet login</h1>
+    <a href="/"><h1 id="logo">rynet login</h1></a>
     <?php if($userutils->loggedIn()) { ?>
-    <form action="/logout">
-        <button>log out</button>
+    <form action="/logout" id="logout_form">
+        <button id="logout">log out</button>
     </form>
     <?php } ?>
 </header>

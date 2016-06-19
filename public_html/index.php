4c02814d8eb4fcdefbcdecfab3d102ce57b5311b
Ryan <ryanf.rowe@gmail.com>
2016-06-19 13:51:05 -0400

    Utilized new secure method

--- public_html/index.php
+++ public_html/index.php
@@ -3,13 +3,8 @@
 require($_SERVER["DOCUMENT_ROOT"]."/utils/UserUtils.php");
 $userutils = new UserUtils();
 
-if($userutils->loggedIn()) {
-    echo "Welcome, " . $userutils->name();
-} else {
-    redirectLogin();
-}
+$userutils->secure("redirectLogin");
 
 function redirectLogin() {
-    header("Location: /login/");
+    header("Location: /status/");
 }
-

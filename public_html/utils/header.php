9b62cc1116f097812f626adf82c9d4d479fd1700
Ryan <ryanf.rowe@gmail.com>
2016-06-19 13:56:56 -0400

    Logout using post

--- public_html/utils/header.php
+++ public_html/utils/header.php
@@ -1,7 +1,7 @@
 <header>
     <a href="/"><h1 id="logo">rynet login</h1></a>
     <?php if($userutils->loggedIn()) { ?>
-    <form action="/logout" id="logout_form">
+    <form action="/logout" id="logout_form" method="post">
         <button id="logout">log out</button>
     </form>
     <?php } ?>

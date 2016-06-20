98557f0023bb58d2873a20269ccce3d175095226
Ryan <ryanf.rowe@gmail.com>
2016-06-19 22:49:29 -0400

    Added viewport to imports

--- public_html/utils/imports.php
+++ public_html/utils/imports.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="/css/style.css" >
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 <script>
